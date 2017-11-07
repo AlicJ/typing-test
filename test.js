ac85a37dcda9095d7705b30baf2d83a140da0174
alicj <zichen.jiang@yahoo.com>
2017-11-06 23:10:55 -0500

    add test caes and users

--- test.js
+++ test.js
@@ -8,12 +8,35 @@ var newTestButton = document.getElementById("newTestButton");
 var score = 0;
 var stop = false;
 
+var KEYBOARD = ["O", "S", "T"];
+var PENALTY = [0, 4.5, 9];
+var TESTCASES = [];
+var USERS = [];
+
+for (var i = 0; i < KEYBOARD.length; i++) {
+	for (var j = 0; j < PENALTY.length; j++) {
+		console.log(i, j)
+		TESTCASES.push({id: i * 3 + j, keyboard: KEYBOARD[i], penalty: PENALTY[j] })
+	}
+}
+
+for (var i = 0; i < 9; i++) {
+	USERS.push({id: i, testcases: []});
+	for (var j = i; j < i+5; j++) {
+		if (j < TESTCASES.length) {
+			USERS[i]['testcases'].push(TESTCASES[j]);
+		} else {
+			USERS[i]['testcases'].push(TESTCASES[j-9]);
+		}
+	}
+}
+
 var generatePassage = function(){
 
 	paraContainer.style.display = "inline-block";
 	textArea.style.display = "inline-block";
 	// testPara.innerHTML = "Friends are flowers in the garden of life.";
-	testPara.innerHTML = "F";
+	testPara.innerHTML = "Friends are great.";
 };
 
 var displayTime = function () {
@@ -30,7 +53,8 @@ var displayTime = function () {
 			document.body.appendChild(newTestWindow);
 			document.getElementById("finalScore").innerHTML = "Your took: " + timeRemaining + " milliseconds";
 			newTestWindow.style.display = "block";
-			
+			//subtract 20ms per character?
+
 		} else{
 			if(testPara.innerHTML == textArea.value){
 				stop = true;
